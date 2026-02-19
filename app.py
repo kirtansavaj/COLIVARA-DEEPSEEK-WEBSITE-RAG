c64f1757b6a5cf696bff6d0f8a70f8820c71288a
kirtansavaj <102339342+kirtansavaj@users.noreply.github.com>
2026-02-19 21:13:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 # Adapted from https://docs.streamlit.io/knowledge-base/tutorials/build-conversational-apps#build-a-simple-chatbot-gui-with-streaming
 import os
-
 import base64
 import gc
 import random
